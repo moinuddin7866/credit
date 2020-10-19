abb145dfbb822a27962d5f1aa3ab8c1178244c5a
MD Khaja Moinuddin <70834628+moinuddin7866@users.noreply.github.com>
2020-10-19 22:52:11 +0530

    commit

--- task2/settings.py
+++ task2/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'task2.urls'
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -123,11 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [
+#if DEBUG:
+STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
 #else:
- #   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+ #   
